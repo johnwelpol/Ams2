1267802410f0d8a4003573633a500959c1bacf6d
Johnwel <johnwellibariospol@gmail.com>
2025-03-02 14:53:14 +0800

    add ziggy setup

---
+++ resources/js/types/ziggy.d.ts
@@ -0,0 +1,8 @@
+declare module '../../vendor/tightenco/ziggy' {
+    const Ziggy: any;
+    export default Ziggy;
+}
+declare module '../../vendor/tightenco/ziggy/dist/index' {
+    const route: any;
+    export default route;
+}
